2ae7107b29e6999a74c73e083f5ef45d345d7e70
Naeem009 <m.n.shahzad@gmail.com>
2024-05-09 23:21:18 +0500

    Update page.tsx

--- projectshadcn/src/app/page.tsx
+++ projectshadcn/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Design Page with ShadCN UI</h1>
+      <div><h1>Design Page with ShadCN UI</h1></div>
+      <button>Test Button</button>
     </main>
   );
 }
